91966bb765fa3176051610722af15ef882135a6f
Mahi-693 <hhmmmahi2004@gmail.com>
2026-03-07 23:17:32 +0600

    refactor: clean up VirtualDisk binary read/write interface

--- src/VirtualDisk.cpp
+++ src/VirtualDisk.cpp
@@ -7,8 +7,8 @@ using namespace std;
 // Constructor
 VirtualDisk::VirtualDisk(const string& fileName, unsigned int size) 
     : diskFileName(fileName), diskSize(size), isOpen(false) {
-    cout << "[VirtualDisk] Initialized with file: " << fileName 
-         << ", size: " << size << " bytes" << endl;
+    // cout << "[VirtualDisk] Initialized with file: " << fileName
+    //      << ", size: " << size << " bytes" << endl;
 }
 
 // Destructor
@@ -16,12 +16,12 @@ VirtualDisk::~VirtualDisk() {
     if (isOpen) {
         close();
     }
-    cout << "[VirtualDisk] Destroyed" << endl;
+    // cout << "[VirtualDisk] Destroyed" << endl;
 }
 
 // Create a new disk file
 bool VirtualDisk::create() {
-    cout << "[VirtualDisk] Creating new disk file: " << diskFileName << endl;
+    // cout << "[VirtualDisk] Creating new disk file: " << diskFileName << endl;
     
     // Open file in write mode
     diskFile.open(diskFileName, ios::out | ios::binary);
@@ -38,7 +38,7 @@ bool VirtualDisk::create() {
     unsigned int blocksToWrite = diskSize / 4096;
     unsigned int remainder = diskSize % 4096;
     
-    cout << "[VirtualDisk] Writing " << blocksToWrite << " blocks of 4096 bytes" << endl;
+    // cout << "[VirtualDisk] Writing " << blocksToWrite << " blocks of 4096 bytes" << endl;
     
     for (unsigned int i = 0; i < blocksToWrite; i++) {
         diskFile.write(zeroBlock, 4096);
@@ -53,12 +53,12 @@ bool VirtualDisk::create() {
     // Write remaining bytes
     if (remainder > 0) {
         diskFile.write(zeroBlock, remainder);
-        cout << "[VirtualDisk] Wrote " << remainder << " remaining bytes" << endl;
+        // cout << "[VirtualDisk] Wrote " << remainder << " remaining bytes" << endl;
     }
     
     diskFile.close();
     
-    cout << "[VirtualDisk] Disk file created successfully" << endl;
+    // cout << "[VirtualDisk] Disk file created successfully" << endl;
     
     // Now open it for read/write
     return open();
@@ -66,7 +66,7 @@ bool VirtualDisk::create() {
 
 // Open existing disk file
 bool VirtualDisk::open() {
-    cout << "[VirtualDisk] Opening disk file: " << diskFileName << endl;
+    // cout << "[VirtualDisk] Opening disk file: " << diskFileName << endl;
     
     diskFile.open(diskFileName, ios::in | ios::out | ios::binary);
     
@@ -86,14 +86,14 @@ bool VirtualDisk::open() {
     }
     
     isOpen = true;
-    cout << "[VirtualDisk] Disk file opened successfully" << endl;
+    // cout << "[VirtualDisk] Disk file opened successfully" << endl;
     return true;
 }
 
 // Close disk file
 void VirtualDisk::close() {
     if (isOpen) {
-        cout << "[VirtualDisk] Closing disk file" << endl;
+        // cout << "[VirtualDisk] Closing disk file" << endl;
         diskFile.close();
         isOpen = false;
     }
@@ -112,7 +112,7 @@ bool VirtualDisk::readBlock(unsigned int offset, char* buffer, unsigned int size
         return false;
     }
     
-    cout << "[VirtualDisk] Reading " << size << " bytes from offset " << offset << endl;
+    // cout << "[VirtualDisk] Reading " << size << " bytes from offset " << offset << endl;
     
     diskFile.seekg(offset, ios::beg);
     diskFile.read(buffer, size);
@@ -138,7 +138,7 @@ bool VirtualDisk::writeBlock(unsigned int offset, const char* buffer, unsigned i
         return false;
     }
     
-    cout << "[VirtualDisk] Writing " << size << " bytes to offset " << offset << endl;
+    // cout << "[VirtualDisk] Writing " << size << " bytes to offset " << offset << endl;
     
     diskFile.seekp(offset, ios::beg);
     diskFile.write(buffer, size);
@@ -164,7 +164,7 @@ bool VirtualDisk::getDiskStatus() const {
 // Flush disk changes
 void VirtualDisk::flush() {
     if (isOpen) {
-        cout << "[VirtualDisk] Flushing disk changes" << endl;
+        // cout << "[VirtualDisk] Flushing disk changes" << endl;
         diskFile.flush();
     }
 }
